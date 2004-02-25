fc538709a371c4730d0cabd1131fd2c17cc941ec
Kaleb Keithley <kaleb@freedesktop.org>
2004-02-25 21:46:48 +0000

    bug #230 Revert to Xinerama 1.1 In order to make a "quick" release it has     been decided that the priority is to preserve the server's internal     API/ABI so that third-party drivers that depend on symbols like     noPanoramiXExtension, etc., would not need to be recompiled. Too bad     gcc on Linux doesn't support ELF's weak symbols as that would have been     a reasonable solution for preserving the ABI. N.B.: While symbols, i.e.     functions and variables revert to the old name, I did not revert build     names, i.e. -DXINERAMA, to the old -DPANORAMIX. There was no need, and     it's just a build issue that has no impact on the binary output of the     build.

--- src/apm_rush.c
+++ src/apm_rush.c
@@ -1,4 +1,4 @@
-/* $XdotOrg$ */
+/* $XdotOrg: xc/programs/Xserver/hw/xfree86/drivers/apm/apm_rush.c,v 1.1.4.1 2003/12/18 19:29:14 kaleb Exp $ */
 /* $XFree86: xc/programs/Xserver/hw/xfree86/drivers/apm/apm_rush.c,v 1.12 2003/02/12 21:46:42 tsi Exp $ */
 /*
  * Copyright Loïc Grenié 1999
@@ -347,7 +347,7 @@ XFree86RushExtensionInit(ScreenPtr pScreen)
     ExtensionEntry* extEntry;
 
 #ifdef XINERAMA
-    if (!noXineramaExtension)
+    if (!noPanoramiXExtension)
 	return;
 #endif
     if (rush_ext_generation == serverGeneration) {
