bebcaf61cee6e3bea9477ded1a55ce7ea33130d3
Enrico Weigelt, metux IT consult <info@metux.net>
2025-12-18 12:00:35 +0100

    drop support for ancient pre-7.1 xextproto
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- src/apm_driver.c
+++ src/apm_driver.c
@@ -6,13 +6,7 @@
 #include "xf86int10.h"
 #include "vbe.h"
 
-#ifdef HAVE_XEXTPROTO_71
 #include <X11/extensions/dpmsconst.h>
-#else
-#define DPMS_SERVER
-#include <X11/extensions/dpms.h>
-#endif
-
 
 #define APM_VERSION		4000
 #define APM_NAME		"APM"
